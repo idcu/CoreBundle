60c43aa4beff7a31867064db2208444c8343cc81
idcu <it@idcu.uu.me>
2016-05-24 22:13:41 +0800

    IDCUCoreBundle

---
+++ Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace IDCU\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('IDCUCoreBundle:Default:index.html.twig');
+    }
+}
---
+++ IDCUCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace IDCU\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IDCUCoreBundle extends Bundle
+{
+}
